168002a876ee5889745855d711cd2c9051ba6743
m-radman <milrad@live.com>
2023-01-23 18:53:15 +0100

    add key presses tests

--- test/specs/key_presses.e2e.ts
+++ test/specs/key_presses.e2e.ts
@@ -1,1 +1,50 @@
 // todo 
+import { Key } from "webdriverio"
+
+const BASE_URL = "https://the-internet.herokuapp.com"
+
+describe("key presses tests", () => {
+  it("should detect that 'Tab' key is pressed", async () => {
+    await browser.url(`${BASE_URL}/key_presses?`)
+
+    let input = await $("input[id='target]")
+    await browser.keys(Key.Tab)
+
+    let result = await (await $("p[id='result']")).getText()
+
+    expect(result).toBe("You entered: TAB")
+  })
+
+  it("should detect that 'Space' key is pressed", async () => {
+    await browser.url(`${BASE_URL}/key_presses?`)
+
+    let input = await $("input[id='target]")
+    await browser.keys(Key.Space)
+
+    let result = await (await $("p[id='result']")).getText()
+
+    expect(result).toBe("You entered: SPACE")
+  })
+
+  it("should inform which key is pressed last", async () => {
+    await browser.url(`${BASE_URL}/key_presses?`)
+
+    let input = await $("input[id='target]")
+    await browser.keys(["a", "b", "c", "d"])
+
+    let result = await (await $("p[id='result']")).getText()
+
+    expect(result).toBe("You entered: D")
+  })
+
+  it("should not recognise semicolon", async () => {
+    await browser.url(`${BASE_URL}/key_presses?`)
+
+    let input = await $("input[id='target]")
+    await browser.keys(Key.Semicolon)
+
+    let result = await (await $("p[id='result']")).getText()
+
+    expect(result).toBe("You entered:")
+  })
+})
