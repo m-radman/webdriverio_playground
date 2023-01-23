31e377f874782089e975394fb744b200f374ba12
Iva Cizmas <ivacizmas@gmail.com>
2023-01-23 21:27:47 +0100

    tests for inputs

--- test/specs/inputs.e2e.ts
+++ test/specs/inputs.e2e.ts
@@ -1,1 +1,8 @@
-// todo 
+const BASE_URL = "https://the-internet.herokuapp.com"
+
+describe("Inputs tests", () => {
+  it("user should be able to set input 0", async () => {
+    await browser.url(`${BASE_URL}/checkboxes`)
+    // ...  
+  })  
+})
