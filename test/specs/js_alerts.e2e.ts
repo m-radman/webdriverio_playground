1adfdbb616b0163b2fa68684a531c396f0bc47c4
m-radman <milrad@live.com>
2023-01-24 08:50:19 +0100

    add tests fot JS confirm alert and JS prompt

--- test/specs/js_alerts.e2e.ts
+++ test/specs/js_alerts.e2e.ts
@@ -1,5 +1,5 @@
 // todo 
-describe("input type number tests", () => {
+describe("JS alerts tests", () => {
   const BASE_URL = "https://the-internet.herokuapp.com"
 
   it("should successfuly click on the alert", async () => {
@@ -12,5 +12,66 @@ describe("input type number tests", () => {
     }
 
     expect(await (await $("p[id='result']")).getText()).toBe("You successfully clicked an alert")
+  })
+
+  it("should click 'OK' on the alert", async () => {
+    await browser.url(`${BASE_URL}/javascript_alerts`)
+
+    await (await $("button=Click for JS Confirm")).click()
+    
+    if (await browser.isAlertOpen()) {
+      await browser.acceptAlert()
+    }
+
+    expect(await (await $("p[id='result']")).getText()).toBe("You clicked: Ok")
+  })
+
+  it("should click 'Cancel' on the alert", async () => {
+    await browser.url(`${BASE_URL}/javascript_alerts`)
+
+    await (await $("button=Click for JS Confirm")).click()
+    
+    if (await browser.isAlertOpen()) {
+      await browser.dismissAlert()
+    }
+
+    expect(await (await $("p[id='result']")).getText()).toBe("You clicked: Cancel")
+  })
+
+  it("should accept input in prompt", async () => {
+    await browser.url(`${BASE_URL}/javascript_alerts`)
+
+    await (await $("button=Click for JS Prompt")).click()
+    
+    if (await browser.isAlertOpen()) {
+      await browser.sendAlertText("Hello!")
+      await browser.acceptAlert()
+    }
+
+    expect(await (await $("p[id='result']")).getText()).toBe("You entered: Hello!")
+  })
+
+  it("should accept blank input in prompt", async () => {
+    await browser.url(`${BASE_URL}/javascript_alerts`)
+
+    await (await $("button=Click for JS Prompt")).click()
+    
+    if (await browser.isAlertOpen()) {
+      await browser.acceptAlert()
+    }
+
+    expect(await (await $("p[id='result']")).getText()).toBe("You entered:")
+  })
+
+  it("should cancel prompt", async () => {
+    await browser.url(`${BASE_URL}/javascript_alerts`)
+
+    await (await $("button=Click for JS Prompt")).click()
+    
+    if (await browser.isAlertOpen()) {
+      await browser.dismissAlert()
+    }
+
+    expect(await (await $("p[id='result']")).getText()).toBe("You entered: null")
   })
 })
