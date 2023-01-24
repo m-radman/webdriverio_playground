af3d108cfdaaf00d22101733c8cfa232cfc1c941
m-radman <milrad@live.com>
2023-01-24 17:47:37 +0100

    add test dynamic_loading.embedded_element.e2e.ts

--- test/specs/dynamic_loading.embedded_element.e2e.ts
+++ test/specs/dynamic_loading.embedded_element.e2e.ts
@@ -1,1 +1,19 @@
 // todo 
+describe("test for dynamic loading with rendered element", () => {
+  const BASE_URL = "https://the-internet.herokuapp.com"
+
+  it("test for dynamic loading with rendered element", async () => {
+    await browser.url(`${BASE_URL}/dynamic_loading`)
+
+    await (await $("#content > div > a:nth-child(8)")).click()
+
+    expect(await (await $("#finish")).isExisting()).toBe(false)
+
+    await (await $("#start > button")).click()
+    await (await $("#finish")).waitForDisplayed()
+
+    expect(await (await $("#finish")).isExisting()).toBe(true)
+    expect(await (await $("#finish")).isDisplayedInViewport()).toBe(true)
+    expect(await (await $("#start > button")).isDisplayedInViewport()).toBe(false)
+  })
+})
