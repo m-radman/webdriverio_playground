6067362f6a6780b780ec373d9de2d3d88df6798e
Iva Cizmas <ivacizmas@gmail.com>
2023-01-24 22:46:27 +0100

    [e2e] adding file_upload.e2e.ts e2e test

--- test/specs/file_upload.e2e.ts
+++ test/specs/file_upload.e2e.ts
@@ -1,1 +1,32 @@
-// todo 
+import * as EC from 'wdio-wait-for'
+
+const BASE_URL = "https://the-internet.herokuapp.com"
+
+/**
+ * Note: See this example https://webdriver.io/docs/api/browser/uploadFile#example
+ */
+describe("File upload tests", () => {
+  it("user should be able to upload file", async () => {
+    await browser.url(`${BASE_URL}/upload`)
+
+    /**
+     * make sure you have some file in repo to use it for upload 
+     */
+    const filePath = './assets/images/test.png'
+    const remoteFilePath = await browser.uploadFile(filePath)
+
+    const fileUploadInput = $('input#file-upload')
+    const fileUploadBtn = $('input#file-submit')
+
+    await fileUploadInput.setValue(remoteFilePath)
+    await fileUploadBtn.click()
+
+    const h3 = $('div.example > h3')
+    await browser.waitUntil(EC.textToBePresentInElement(h3, "File Uploaded!"))
+
+    const uploadedFile  = $('div.example > div#uploaded-files')
+    expect(await uploadedFile.getText()).toBe('test.png')
+  })
+
+  // todo - upload  pdf file 
+})
