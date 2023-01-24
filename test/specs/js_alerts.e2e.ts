ea83767b723cdb14513a811d7f747ebbe1b347cd
m-radman <milrad@live.com>
2023-01-24 08:27:37 +0100

    add test for 'Click for JS Alert' button

--- test/specs/js_alerts.e2e.ts
+++ test/specs/js_alerts.e2e.ts
@@ -1,1 +1,16 @@
 // todo 
+describe("input type number tests", () => {
+  const BASE_URL = "https://the-internet.herokuapp.com"
+
+  it("should successfuly click on the alert", async () => {
+    await browser.url(`${BASE_URL}/javascript_alerts`)
+
+    await (await $("button=Click for JS Alert")).click()
+    
+    if (await browser.isAlertOpen()) {
+      await browser.acceptAlert()
+    }
+
+    expect(await (await $("p[id='result']")).getText()).toBe("You successfully clicked an alert")
+  })
+})
