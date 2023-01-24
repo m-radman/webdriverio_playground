e13a3847b79138ac2da5df2d03e3322aae652a8f
m-radman <milrad@live.com>
2023-01-24 17:30:43 +0100

    add test dynamic_loading.hidden_element.e2e.ts

--- test/specs/dynamic_loading.hidden_element.e2e.ts
+++ test/specs/dynamic_loading.hidden_element.e2e.ts
@@ -1,1 +1,16 @@
 // todo 
+describe("test for dynamic loading with hidden element", () => {
+  const BASE_URL = "https://the-internet.herokuapp.com"
+
+  it("test for dynamic loading with hidden element", async () => {
+    await browser.url(`${BASE_URL}/dynamic_loading`)
+
+    await (await $("#content > div > a:nth-child(5)")).click()
+
+    await (await $("#start > button")).click()
+    await (await $("#finish > h4")).waitForDisplayed()
+
+    expect(await (await $("#finish > h4")).isDisplayedInViewport()).toBe(true)
+    expect(await (await $("#start > button")).isDisplayedInViewport()).toBe(false)
+  })
+})
