9c62f38b541dcf9f8b5950b5c859ff91bafeb188
m-radman <milrad@live.com>
2023-01-25 08:33:49 +0100

    add some comments to dropdown.e2e.ts

--- test/specs/dropdown.e2e.ts
+++ test/specs/dropdown.e2e.ts
@@ -5,6 +5,7 @@ describe("dropdown tests", () => {
   it("user should be able to select option 1", async () => {
     await browser.url(`${BASE_URL}/dropdown`)
     expect(await $("#dropdown > option:nth-child(1)").isSelected()).toBe(true)
+    //or expect(await $("#dropdown > option:nth-child(1)").getAttribute('selected')).toBe('selected')
 
     await $("#dropdown").click()
     await $("#dropdown > option:nth-child(2)").click()
@@ -25,5 +26,6 @@ describe("dropdown tests", () => {
   it("user should not be able to select disabled option 0", async () => {
     await browser.url(`${BASE_URL}/dropdown`)
     expect(await $("#dropdown > option:nth-child(1)").isEnabled()).toBe(false)
+    //or expect(await $("#dropdown > option:nth-child(1)").getAttribute('disabled')).toBe('disabled')
   })
 })
