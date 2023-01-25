27cfe64034400514c0d4d4db553f1bfc98f4ae27
m-radman <milrad@live.com>
2023-01-25 10:59:02 +0100

    add test redirector.e2e.ts

--- test/specs/redirector.e2e.ts
+++ test/specs/redirector.e2e.ts
@@ -1,1 +1,12 @@
 // todo 
+describe("Checkboxes tests", () => {
+  const BASE_URL = "https://the-internet.herokuapp.com"
+
+  it("should redirect user to status codes page", async () => {
+    await browser.url(`${BASE_URL}/redirector`)
+
+    await $("p > #redirect").click()
+
+    expect(browser).toHaveUrl(`${BASE_URL}/status_codes`)
+  })
+})
