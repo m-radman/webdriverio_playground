1cc0af0adba70c3cd730707260cbd0d9247bd10f
Iva Cizmas <ivacizmas@gmail.com>
2023-01-25 21:02:14 +0100

    redirector.e2e.test added comments + headless option commented out

--- test/specs/redirector.e2e.ts
+++ test/specs/redirector.e2e.ts
@@ -1,12 +1,12 @@
-// todo 
-describe("Checkboxes tests", () => {
+describe("Redirector tests", () => {
   const BASE_URL = "https://the-internet.herokuapp.com"
 
   it("should redirect user to status codes page", async () => {
     await browser.url(`${BASE_URL}/redirector`)
 
+    // click redirect link 
     await $("p > #redirect").click()
-
+    // expect to be redirected to /status_code url
     expect(browser).toHaveUrl(`${BASE_URL}/status_codes`)
   })
 })
