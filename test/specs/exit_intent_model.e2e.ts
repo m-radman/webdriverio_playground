d6c1f1644db25a76e0e6d6f8c0f153d7bc345ddf
m-radman <milrad@live.com>
2023-01-25 23:32:59 +0100

    add test exit_intent_model.e2e.ts

--- test/specs/exit_intent_model.e2e.ts
+++ test/specs/exit_intent_model.e2e.ts
@@ -1,1 +1,15 @@
 // todo 
+
+const BASE_URL = "https://the-internet.herokuapp.com"
+
+describe("Exit intent test", () => {
+  it("should activate modal window", async () => {
+    await browser.url(`${BASE_URL}/exit_intent`)
+
+    expect(await $("#ouibounce-modal").getAttribute("style")).toBe("display:none;")
+    //simulate trigger event
+    await browser.execute("window._ouibounce.fire()")
+
+    expect(await $("#ouibounce-modal").getAttribute("style")).toBe("display: block;")
+  })
+})
