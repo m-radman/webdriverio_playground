09a14987aa7c409dbb73bd43cefe90a6c633a587
m-radman <milrad@live.com>
2023-01-28 08:39:30 +0100

    refactor checkbox page object

--- test/pageobjects/CheckboxPage.ts
+++ test/pageobjects/CheckboxPage.ts
@@ -1,32 +1,18 @@
+import { CheckboxInput } from "./common/CheckboxInput.js"
+
 class CheckboxPage {
   static url: string = "https://the-internet.herokuapp.com/checkboxes"
-  
-  private checkbox1: string
-  private checkbox2: string
 
+  public checkbox1: CheckboxInput
+  public checkbox2: CheckboxInput
+  
   constructor() {
-    this.checkbox1 = "#checkboxes > input:nth-of-type(1)"
-    this.checkbox2 = "#checkboxes > input:nth-of-type(2)"
+    this.checkbox1 = new CheckboxInput("input[type='checkbox']:nth-of-type(1)")
+    this.checkbox2 = new CheckboxInput("input[type='checkbox']:nth-of-type(2)")
   }
 
   async open() {
     await browser.url(CheckboxPage.url)
-  }
-
-  async getCheckbox1Status(): Promise<boolean> {
-    return $(this.checkbox1).isSelected()
-  }
-
-  async getCheckbox2Status(): Promise<boolean> {
-    return $(this.checkbox2).isSelected()
-  }
-
-  async clickCheckbox1() {
-    await $(this.checkbox1).click()
-  }
-
-  async clickCheckbox2() {
-    await $(this.checkbox2).click()
   }
 }
 
